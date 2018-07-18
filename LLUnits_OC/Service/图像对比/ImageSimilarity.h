48ca7177b7288fb26265c4a7b273fba1b3edc1d5
luoliu <luoliu@happymilk.ltd>
2018-07-18 17:59:08 +0800

    ImageSimilarity

---
+++ LLUnits_OC/Service/图像对比/ImageSimilarity.h
@@ -0,0 +1,23 @@
+//
+//  ImageSimilarity.h
+//  iManager
+//
+//  Created by mac on 2018/7/6.
+//  Copyright © 2018年 luoliu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ImageSimilarity : NSObject
+
+// 直接对比图片
++ (BOOL)isSimilarityValueWithImgA:(UIImage*)imgA imgB:(UIImage*)imgB;
+
+// 对比图片的hash值
++ (BOOL)isSimilarityValueWithHashA:(NSString *)hashA hashB:(NSString *)hashB;
+
+// 获取图片的hash值
++ (NSString *)getHashValue:(UIImage *)img;
+
+@end
